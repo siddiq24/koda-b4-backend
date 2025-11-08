862ccb604779e9a2b7ec6aed6bb2de4e4f7ffca1
siddiq24 <siddiqrm24@gmail.com>
2025-11-09 03:15:46 +0700

    feat(router): add route to open view swagger

--- routers/init_router.go
+++ routers/init_router.go
@@ -3,7 +3,10 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/siddiq24/backend-coffee-shop/docs"
 	"github.com/siddiq24/backend-coffee-shop/middlewares"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter(pg *pgxpool.Pool) *gin.Engine {
@@ -13,6 +16,8 @@ func InitRouter(pg *pgxpool.Pool) *gin.Engine {
 	AuthRouter(r, pg)
 	PromoRouter(r, pg)
 	ProductRouter(r, pg)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return r
 }
