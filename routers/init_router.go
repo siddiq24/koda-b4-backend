2d725102b437fb23901b2b94e7c479f45e8b7ad2
siddiq24 <siddiqrm24@gmail.com>
2025-11-08 14:08:08 +0700

    feat: implement cors to all routers

--- routers/init_router.go
+++ routers/init_router.go
@@ -3,10 +3,14 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/siddiq24/backend-coffee-shop/middlewares"
 )
 
 func InitRouter(pg *pgxpool.Pool) *gin.Engine {
 	r := gin.Default()
+	r.Use(middlewares.Cors())
+
+	AuthRouter(r, pg)
 
 	return r
 }
