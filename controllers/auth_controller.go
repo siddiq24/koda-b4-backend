0d15440da73891b3db64f01fb67e07c105c9fb04
siddiq24 <siddiqrm24@gmail.com>
2025-11-08 13:29:33 +0700

    feat(controller); add controller to login user

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,11 +1,11 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/siddiq24/backend-coffee-shop/libs"
 	"github.com/siddiq24/backend-coffee-shop/models"
 )
 
@@ -29,17 +29,39 @@ func (a *AuthController) Register(c *gin.Context) {
 	if request.Role == "" {
 		request.Role = "user"
 	}
+	request.Password = libs.Create_Hash(request.Password)
 
-	if a.Model.UserExist(c, request.Email) {
+	if a.Model.EmailExist(c, request.Email) > 0 {
 		models.ErrorResponse(c, http.StatusConflict, "Silahkan login atau gunakan email lain", "Email telah terdaftar")
 		return
 	}
 
-	id, _ := a.Model.AddUser(c, request)
-	log.Println(id)
+	if _, err := a.Model.AddUser(c, request); err != nil {
+		models.ErrorResponse(c, http.StatusConflict, "Terjadi kesalahan saat menambahkan user", err.Error())
+		return
+	}
+
 	c.JSON(http.StatusCreated, models.JSON_Response{
 		Success: true,
 		Message: "Register successfully",
 		Result:  request,
 	})
+}
+
+func (a *AuthController) Login(c *gin.Context) {
+	var req models.AuthRequest
+	if err := c.ShouldBindBodyWith(&req, binding.JSON); err != nil {
+		models.ErrorResponse(c, http.StatusBadRequest, "Bad Request", err.Error())
+		return
+	}
+
+	if libs.Verify_Hash(req.Password, a.Model.PasswordUser(c, req.Email)) {
+		c.JSON(http.StatusCreated, models.JSON_Response{
+			Success: true,
+			Message: "Register successfully",
+			Result:  req,
+		})
+	}
+
+	models.ErrorResponse(c, http.StatusBadRequest, "Password wrong", "")
 }
