ca604dd077113e1534584d223486a9432b78839c
siddiq24 <siddiqrm24@gmail.com>
2025-11-09 03:14:38 +0700

    feat(main): add comment for documentation swagger

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/siddiq24/backend-coffee-shop/configs"
+	_ "github.com/siddiq24/backend-coffee-shop/docs"
 	"github.com/siddiq24/backend-coffee-shop/routers"
 )
 
+// @title           Backend User Management API
+// @version         1.0
+// @description     API for user management and authentication
+// @host      		localhost:8085
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer" followed by a space and JWT token.
+// @schemes http https
+// @tag.name authentication
+// @tag.description Authentication endpoints (register, login, logout)
+// @tag.name users
+// @tag.description User management endpoints (CRUD operations)
+// @BasePath  /
 func main() {
 	godotenv.Load()
-
 	pg := configs.InitDbConfigs()
 	r := routers.InitRouter(pg)
 
