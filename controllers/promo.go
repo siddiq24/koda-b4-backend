bbc1ee118458b9a56aae5a3b69126c0ac347f293
siddiq24 <siddiqrm24@gmail.com>
2025-11-08 15:29:14 +0700

    feat(controller): add controller to get all promo

---
+++ controllers/promo.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/siddiq24/backend-coffee-shop/models"
+)
+
+type Promo struct {
+	Pg *pgxpool.Pool
+}
+
+func (p *Promo) GetPromo(c *gin.Context) {
+	promos, err := models.AllPromo(c, p.Pg)
+	if err != nil {
+		models.ErrorResponse(c, http.StatusInternalServerError, "Server error", err)
+	}
+
+	c.JSON(http.StatusOK, promos)
+}
