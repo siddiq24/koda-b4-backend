7798f5bfeaf203af3ce590684d98fb04d7a08024
siddiq24 <siddiqrm24@gmail.com>
2025-11-09 15:23:06 +0700

    refactor(middleware): add allow headers, expose headers, and allow credentials

--- middlewares/CORS.go
+++ middlewares/CORS.go
@@ -9,8 +9,10 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins: []string{os.Getenv("PORT_ACCESS")},
-		AllowMethods: []string{"GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"},
-		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		AllowOrigins:     []string{os.Getenv("PORT_ACCESS"), "http://www.localhost:8085"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
 	})
 }
