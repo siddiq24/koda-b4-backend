cd19e06223964454f4083ec1845dadcf2b2bd083
siddiq24 <siddiqrm24@gmail.com>
2025-11-10 04:19:48 +0700

    fix(libs): enhance generator to add role to claims

--- libs/jwt.go
+++ libs/jwt.go
@@ -1,6 +1,8 @@
 package libs
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"time"
 
@@ -9,18 +11,36 @@ import (
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
-func GenerateJwt(idUser int) (string, error) {
+func GenerateJwt(idUser int, role string) (string, error) {
 	claims := jwt.MapClaims{
-		"id":  idUser,
-		"exp": time.Now().Add(time.Hour).Unix(),
+		"id":   idUser,
+		"role": role,
+		"exp":  time.Now().Add(time.Hour).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtSecret)
 }
 
-func VerifyJwt(token string) (*jwt.Token, error) {
-	return jwt.Parse(token, func(t *jwt.Token) (any, error) {
+func VerifyJwt(tokenString string) (*jwt.MapClaims, error) {
+	if tokenString == "" {
+		return nil, errors.New("token kosong")
+	}
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
 		return jwtSecret, nil
 	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return &claims, nil
+	}
+
+	return nil, errors.New("token tidak valid")
 }
