5fb655e78a641e4f3ac0d337b85a0644954d3d38
siddiq24 <siddiqrm24@gmail.com>
2025-11-10 21:40:55 +0700

    feat(router): add router and made group for admin endpoint

--- routers/admin.go
+++ routers/admin.go
@@ -7,10 +7,37 @@ import (
 )
 
 func AdminRouter(r *gin.Engine) {
-	c := controllers.AdminController{}
+	ac := controllers.AdminController{}
 	admin := r.Group("/admin")
 	admin.Use(middlewares.AuthMiddleware("admin"))
 	{
-		admin.GET("products", c.GetAllProducts)
+		products := admin.Group("/products")
+		products.GET("", ac.GetAllProducts)
+		products.POST("", ac.CreateProduct)
+		products.PATCH("/:id", ac.UpdateProduct)
+		products.DELETE("/:id", ac.DeleteProduct)
+
+		categories := admin.Group("/categories")
+		categories.GET("", ac.GetAllCategories)
+		categories.POST("", ac.CreateCategory)
+		categories.PATCH("/:id", ac.UpdateCategory)
+		categories.DELETE("/:id", ac.DeleteCategory)
+
+		transactions := admin.Group("/transactions")
+		transactions.GET("", ac.GetAllTransactions)
+		transactions.POST("", ac.CreateTransaction)
+		transactions.PATCH("/:id", ac.UpdateTransaction)
+
+		users := admin.Group("users")
+		users.GET("/users", ac.GetAllUsers)
+		users.POST("/users", ac.CreateUser)
+		users.PATCH("/users/:id", ac.UpdateUser)
+		users.DELETE("/users/:id", ac.DeleteUser)
+
+		images := admin.Group("/products/:id/images")
+		images.GET("/:image_id", ac.GetProductImage)
+		images.POST("", ac.AddProductImage)
+		images.PATCH("/:image_id", ac.UpdateProductImage)
+		images.DELETE("/:image_id", ac.DeleteProductImage)
 	}
 }
