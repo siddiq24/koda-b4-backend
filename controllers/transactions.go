d58dc86ad0d284f18ebf112ee0b8ebcd008ae5dd
siddiq24 <siddiqrm24@gmail.com>
2025-11-13 10:42:28 +0700

    feat(controller): add controller to get history with filter from param and id user

--- controllers/transactions.go
+++ controllers/transactions.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/siddiq24/backend-coffee-shop/libs"
@@ -35,5 +37,57 @@ func (tc TransactionControoller) CreateTransactions(c *gin.Context) {
 	c.JSON(http.StatusOK, models.JSON_Response{
 		Success: true,
 		Message: "create transactions successfull",
+	})
+}
+
+func (h *TransactionControoller) GetHistory(c *gin.Context) {
+	claim, err := libs.VerifyJwt((c.Request.Header.Get("Authorization")[7:]))
+	if err != nil {
+		models.ErrorResponse(c, http.StatusNetworkAuthenticationRequired, "Unauthorize", err.Error())
+		return
+	}
+	uid := int((*claim)["id"].(float64))
+
+	monthStr := c.DefaultQuery("month", "")
+	statusStr := c.DefaultQuery("status", "1")
+	pageStr := c.DefaultQuery("page", "1")
+	limitStr := c.DefaultQuery("limit", "5")
+
+	month, err := strconv.Atoi(monthStr)
+	status, _ := strconv.Atoi(statusStr)
+	page, _ := strconv.Atoi(pageStr)
+	limit, _ := strconv.Atoi(limitStr)
+
+	if page <= 0 {
+		page = 1
+	}
+	if limit <= 5 {
+		limit = 5
+	}
+	if limit >= 10 {
+		limit = 10
+	}
+	if err != nil || monthStr == "" {
+		month = int(time.Now().Month())
+	}
+
+	req := models.History_req{
+		User_id: uid,
+		Month:   month,
+		Status:  status,
+		Page:    page,
+		Limit:   limit,
+	}
+
+	res, err := h.Transaction.GetHistory(c, req)
+	if err != nil {
+		models.ErrorResponse(c, http.StatusInternalServerError, "Service error", err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, models.JSON_Response{
+		Success: true,
+		Message: "Get history successfully",
+		Result:  res,
 	})
 }
