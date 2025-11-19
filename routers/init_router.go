e1e87dfec50fa70f378719e7c19aa9fddb959f48
siddiq24 <siddiqrm24@gmail.com>
2025-11-19 10:25:30 +0700

    refactor: change generate gin in main

--- routers/init_router.go
+++ routers/init_router.go
@@ -5,29 +5,20 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/siddiq24/backend-coffee-shop/docs"
-	"github.com/siddiq24/backend-coffee-shop/middlewares"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-func InitRouter() *gin.Engine {
-	r := gin.Default()
-	if os.Getenv("ENVIRONMENT") == "production" {
-		gin.SetMode(gin.ReleaseMode)
-		r.Use(gin.Recovery())
-	}
-	r.Use(middlewares.Cors())
-
+func InitRouter(r *gin.Engine) {
 	InitWelcomeRouter(r)
 	AuthRouter(r)
 	PromoRouter(r)
 	ProductRouter(r)
-	OrderRouter(r)
 	AdminRouter(r)
 	ProfileRouter(r)
 	TransactionRouter(r)
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	return r
+	if os.Getenv("ENIRONMENT") == "" {
+		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 }
