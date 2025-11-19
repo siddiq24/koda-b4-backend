52fda0aa032bbc26f813347347f41948bc20a43b
siddiq24 <siddiqrm24@gmail.com>
2025-11-19 18:00:13 +0700

    fixing cors using env

--- middlewares/CORS.go
+++ middlewares/CORS.go
@@ -1,13 +1,15 @@
 package middlewares
 
 import (
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{os.Getenv("PORT_ACCESS")},
 		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
