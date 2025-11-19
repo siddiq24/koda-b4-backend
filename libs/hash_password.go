7ad9cca065842c1dd8f25c5ecc354ba57fe7b786
siddiq24 <siddiqrm24@gmail.com>
2025-11-19 10:12:41 +0700

    fix: add output message info error

--- libs/hash_password.go
+++ libs/hash_password.go
@@ -1,27 +1,34 @@
 package libs
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/matthewhartstonge/argon2"
 )
 
-func Create_Hash(pass string) string {
+func Generate_Hash(password string) string {
+	fmt.Println(password)
 	argon := argon2.DefaultConfig()
 
-	encoded, err := argon.HashEncoded([]byte(pass))
+	encoded, err := argon.HashEncoded([]byte(password))
 	if err != nil {
-		log.Println(err)
+		fmt.Printf("failed hashing password: %x", err)
 		return ""
 	}
+
 	return string(encoded)
 }
 
-func Verify_Hash(usePass, dbPass string) bool {
-	log.Println(usePass, "\n", dbPass)
-	ok, err := argon2.VerifyEncoded([]byte(usePass), []byte(dbPass))
+func Verify_Hash(plainPassword, hashedPassword string) bool {
+	fmt.Println(plainPassword, hashedPassword)
+	if hashedPassword == "" {
+		fmt.Printf("hashed password in database is empty")
+		return false
+	}
+
+	ok, err := argon2.VerifyEncoded([]byte(plainPassword), []byte(hashedPassword))
 	if err != nil {
-		log.Println(err)
+		fmt.Printf("failed verifying password: %x", err)
 		return false
 	}
 
