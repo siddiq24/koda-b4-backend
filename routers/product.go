bcf141e95449354d1288bc52637a637246fd9da8
siddiq24 <siddiqrm24@gmail.com>
2025-11-20 13:48:30 +0700

    feat: add endpoint to some detail product

--- routers/product.go
+++ routers/product.go
@@ -11,10 +11,11 @@ func ProductRouter(r *gin.Engine) {
 	r.GET("/products", c.GetAllProducts)
 	r.GET("/products/favorite", c.GetAllFavProducts)
 	r.GET("/products/:id/recomendation", c.GetRekomendasiById)
+	r.GET("/products/:id", c.GetProductByID)
 
 	cart := r.Group("/cart")
 	cart.Use(middlewares.AuthMiddleware("user"))
 	cart.POST("", c.CreateCart)
 	cart.GET("/:id", c.GetProductCart)
-	cart.GET("/list", c.GetfullCard)
+	cart.GET("/list", c.GetFullCart)
 }
