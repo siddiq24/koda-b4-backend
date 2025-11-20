0f3a582f4f2d6905ecd46e9fe81b751f8c48a34b
siddiq24 <siddiqrm24@gmail.com>
2025-11-20 13:46:41 +0700

    feat: improve cors

--- middlewares/CORS.go
+++ middlewares/CORS.go
@@ -8,8 +8,12 @@ import (
 )
 
 func Cors() gin.HandlerFunc {
+	port := os.Getenv("PORT_ACCESS")
+	if port == "" {
+		port = "http://localhost:5173"
+	}
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{os.Getenv("PORT_ACCESS")},
+		AllowOrigins:     []string{port},
 		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
