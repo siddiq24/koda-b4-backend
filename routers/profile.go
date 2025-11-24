6bd4826f717feb870d1aaaa26ece5fbc47b4a6e0
siddiq24 <siddiqrm24@gmail.com>
2025-11-24 11:18:45 +0700

    feat: add allowed role

--- routers/profile.go
+++ routers/profile.go
@@ -8,7 +8,7 @@ import (
 
 func ProfileRouter(r *gin.Engine) {
 	prof := r.Group("/profile")
-	prof.Use(middlewares.AuthMiddleware("user"))
+	prof.Use(middlewares.AuthMiddleware("all"))
 	prof.PATCH("", controllers.UpdateProfile)
 	prof.PATCH("/image", controllers.UpdateProfileImage)
 	prof.GET("", controllers.GetProfileInfo)
